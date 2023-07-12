7998aa9aa15f5459d3d9394a5603a8ab4b923afe
DaffaAdityaDev <daffaaditya990@gmail.com>
2023-07-12 10:05:52 +0700

    update ui

--- src/App.vue
+++ src/App.vue
@@ -93,9 +93,7 @@ onMounted(() => {
 </script>
 
 <template>
-
-
-  <main>
+  <main class="centered-container">
     <HelloWorld msg="Todo List" />
     <Form />
     <Timer :time="currentSelectedTimer" :todo="currentTodo"/>
@@ -104,31 +102,3 @@ onMounted(() => {
 </template>
 
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  /* header { */
-    /* display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  } */
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  /* header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  } */
-}
-</style>
